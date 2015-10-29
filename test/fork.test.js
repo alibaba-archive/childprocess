d864c267799db8fbe2c7bd54b81af743b69e764f
fengmk2 <m@fengmk2.com>
2015-10-29 09:52:21 +0800

    fix: support options.cwd on fork()

--- test/fork.test.js
+++ test/fork.test.js
@@ -1,6 +1,4 @@
 /**!
- * childprocess - test/fork.test.js
- *
  * Copyright(c) node-modules and other contributors.
  * MIT Licensed
  *
@@ -14,6 +12,7 @@
  * Module dependencies.
  */
 
+var pedding = require('pedding');
 var mm = require('mm');
 var assert = require('assert');
 var path = require('path');
@@ -195,6 +194,35 @@ describe('fork()', function () {
       });
     });
 
+  });
+
+  describe('fork with options.cwd', function() {
+    it('should change cwd to test/fixtures/demo', function(done) {
+      var childpath = path.join(__dirname, 'fixtures', 'demo', 'foo.js');
+      var cwd = path.join(__dirname, 'fixtures', 'demo');
+      var child = childprocess.fork(childpath, [], {
+        cwd: cwd,
+      });
+      child.on('exit', function (code) {
+        assert.equal(code, 0);
+        done();
+      });
+    });
+  });
+
+  describe('spawn()', function() {
+    it('should spawn work', function(done) {
+      done = pedding(2, done);
+      var child = childprocess.spawn('echo', ['hi']);
+      child.stdout.on('data', function(data) {
+        assert.equal(data.toString(), 'hi\n');
+        done();
+      });
+      child.on('exit', function (code) {
+        assert.equal(code, 0);
+        done();
+      });
+    });
   });
 
 });
