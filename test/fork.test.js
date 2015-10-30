77a1b8d7870e3fe4bbc6cd18df60e99260281246
popomore <sakura9515@gmail.com>
2015-10-31 02:50:52 +0800

    test: coverage 100%

--- test/fork.test.js
+++ test/fork.test.js
@@ -29,6 +29,7 @@ describe('fork()', function () {
     spy(cp, 'fork');
   });
   afterEach(function() {
+    childprocess.reset();
     cp.fork.reset();
   });
   after(function() {
@@ -52,76 +53,75 @@ describe('fork()', function () {
     });
   });
 
-  describe('inject function', function() {
-
-    before(function() {
-      childprocess.inject(function(modulePath, args, opt) {
-        console.log(modulePath);
-        return [modulePath, args, opt];
-      });
+  it('should work when inject function', function(done) {
+    childprocess.inject(function(modulePath, args, opt) {
+      console.log(modulePath);
+      return [modulePath, args, opt];
     });
-    after(function() {
-      childprocess.reset();
-    });
-
-    it('should work', function(done) {
-      coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
-      .debug()
-      .expect('stdout', /test_fixtures_multi_process_child\.js/)
-      .expect('stdout', /test_fixtures_multi_process_grandchild\.js/)
-      .expect('code', 0)
-      .end(function(err) {
-        assert.ifError(err);
-        assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
-        done();
-      });
+    coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
+    .debug()
+    .expect('stdout', /test_fixtures_multi_process_child\.js/)
+    .expect('stdout', /test_fixtures_multi_process_grandchild\.js/)
+    .expect('code', 0)
+    .end(function(err) {
+      assert.ifError(err);
+      assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
+      done();
     });
   });
 
-  describe('inject jsfile', function() {
-
-    before(function() {
-      childprocess.inject(path.join(__dirname, 'fixtures/inject.js'));
+  it('should work when inject function using os.tmpdir', function(done) {
+    mm(process.env, 'TMPDIR', '');
+    childprocess.inject(function(modulePath, args, opt) {
+      console.log(modulePath);
+      return [modulePath, args, opt];
     });
-    after(function() {
-      childprocess.reset();
-    });
-
-    it('should work', function(done) {
-      coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
-      .expect('stdout', /test_fixtures_multi_process_child\.js/)
-      .expect('stdout', /test_fixtures_multi_process_grandchild\.js/)
-      .expect('code', 0)
-      .end(function(err) {
-        assert.ifError(err);
-        assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
-        done();
-      });
+    coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
+    .debug()
+    .expect('stdout', /test_fixtures_multi_process_child\.js/)
+    .expect('stdout', /test_fixtures_multi_process_grandchild\.js/)
+    .expect('code', 0)
+    .end(function(err) {
+      assert.ifError(err);
+      assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
+      done();
     });
   });
 
-  describe('inject function that returned is not expected', function() {
+  it('should fail', function(done) {
+    childprocess.inject(path.join(__dirname, 'fixtures/inject.js'));
+    coffee.fork(path.join(__dirname, 'fixtures/multi_process/unknown.js'))
+    .debug()
+    .expect('error', /Cannot find module/)
+    .end(done);
+  });
 
-    before(function() {
-      childprocess.inject(function(modulePath) {
-        console.log(modulePath);
-        return [];
-      });
+  it('should work when inject jsfile', function(done) {
+    childprocess.inject(path.join(__dirname, 'fixtures/inject.js'));
+    coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
+    .expect('stdout', /test_fixtures_multi_process_child\.js/)
+    .expect('stdout', /test_fixtures_multi_process_grandchild\.js/)
+    .expect('code', 0)
+    .end(function(err) {
+      assert.ifError(err);
+      assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
+      done();
     });
-    after(function() {
-      childprocess.reset();
-    });
+  });
 
-    it('should work', function(done) {
-      coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
-      .debug()
-      .expect('code', 0)
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.stdout, '');
-        assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
-        done();
-      });
+  it('should inject function that returned is not expected', function(done) {
+    childprocess.inject(function(modulePath) {
+      console.log(modulePath);
+      return [];
+    });
+    coffee.fork(path.join(__dirname, 'fixtures/multi_process/index.js'))
+    .debug()
+    .expect('code', 0)
+    .end(function(err, res) {
+      assert.ifError(err);
+      assert.equal(res.stdout, '');
+      assert.ok(/test\/fixtures\/multi_process\/index\.js/.test(cp.fork.calls[0].arguments[0]));
+      done();
     });
   });
 
